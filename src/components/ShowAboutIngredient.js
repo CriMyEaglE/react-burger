e9b1b600080947615364d90369c8339866d79eae
CriMyEaglE <kulikovisfsb@gmail.com>
2022-10-06 01:37:26 +0700

    fix

---
+++ src/components/ShowAboutIngredient.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export class ShowAboutIngredient extends Component {
+   render() {
+      return (
+         <div>ShowFullIngredient</div>
+      )
+   }
+}
+
+export default ShowAboutIngredient
