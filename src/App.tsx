1b51dfae307a178e1f0acbc55e427fc174d1f0a2
CriMyEaglE <kulikovisfsb@gmail.com>
2022-10-06 22:06:53 +0700

    feat: попапы

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Tab, Logo, Input, BurgerIcon, CloseIcon, CheckMarkIcon, CurrencyIcon, DragIcon, EditIcon, HideIcon, InfoIcon, ListIcon, LockIcon, LogoutIcon, ProfileIcon, ShowIcon, DeleteIcon, ArrowUpIcon, ArrowDownIcon, MenuIcon, ConstructorElement, Button } from '@ya.praktikum/react-developer-burger-ui-components';
-import { BunComponents, MainComponents, SauceComponents, Modal } from './components/api';
+// import { BunComponents, MainComponents, SauceComponents, Modal } from './components/api';
+import { Modal } from './components/api';
+import Ingredients from './components/newApi';
+import done from './image/done.png';
 
 function App() {
   const [current, setCurrent] = React.useState('one')
@@ -40,8 +43,7 @@ function App() {
 
       <div className='main two-columns'>
 
-        <div className='burger-ingredients'>
-
+        <div>
           <h2 style={{ width: '600px', textAlign: 'start' }} className='text text_type_main-large mt-10'>Соберите бургер</h2>
           <div style={{ display: 'flex', alignItems: 'center', height: '56px' }} className='mt-5 tab h-hug'>
             <Tab value="one" active={current === 'one'} onClick={setCurrent}>
@@ -54,20 +56,12 @@ function App() {
               Начинки
             </Tab>
           </div>
-          <h3 style={{ textAlign: 'left', width: '600px', fontSize: '24px' }} className='text text_type_main-default mt-10'>Булки</h3>
-          <div className='burger-ingredients-card'>
-            <BunComponents />
+          <div className='burger-ingredients custom-scroll'>
+            <Ingredients />
           </div>
-          <h3 style={{ textAlign: 'left', width: '600px', fontSize: '24px' }} className='text text_type_main-default mt-10'>Соусы</h3>
-          <div>
-            <SauceComponents />
-          </div>
-          <h3 style={{ textAlign: 'left', width: '600px', fontSize: '24px' }} className='text text_type_main-default mt-10'>Начинки</h3>
-          <div>
-            <MainComponents />
-          </div>
-
         </div>
+
+
 
         <div style={{ display: 'flex', flexDirection: 'column' }} className='mt-25 ml-4 mr-4'>
           <div style={{ height: '80px' }} className='ml-8'>
@@ -79,51 +73,62 @@ function App() {
               thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
             />
           </div>
-          <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
-            <DragIcon type="primary" />
-            <div className='ml-2'>
-              <ConstructorElement
-                text="Краторная булка N-200i (центр)"
-                price={50}
-                thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-              /></div>
-          </div>
-          <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
-            <DragIcon type="primary" />
-            <div className='ml-2'>
-              <ConstructorElement
-                text="Краторная булка N-200i (центр)"
-                price={50}
-                thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-              /></div>
-          </div>
-          <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
-            <DragIcon type="primary" />
-            <div className='ml-2'>
-              <ConstructorElement
-                text="Краторная булка N-200i (центр)"
-                price={50}
-                thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-              /></div>
-          </div>
-          <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
-            <DragIcon type="primary" />
-            <div className='ml-2'>
-              <ConstructorElement
-                text="Краторная булка N-200i (центр)"
-                price={50}
-                thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-              /></div>
-          </div>
-          <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
-            <DragIcon type="primary" />
-            <div className='ml-2'>
-              <ConstructorElement
-                text="Краторная булка N-200i (центр)"
-                price={50}
-                thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
-              /></div>
-          </div>
+          <div className='scroll-block custom-scroll'>
+            <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
+              <div style={{ cursor: 'pointer' }}>
+                <DragIcon type="primary" />
+              </div>
+              <div className='ml-2'>
+                <ConstructorElement
+                  text="Краторная булка N-200i (центр)"
+                  price={50}
+                  thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
+                /></div>
+            </div>
+            <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
+              <div style={{ cursor: 'pointer' }}>
+                <DragIcon type="primary" />
+              </div>
+              <div className='ml-2'>
+                <ConstructorElement
+                  text="Краторная булка N-200i (центр)"
+                  price={50}
+                  thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
+                /></div>
+            </div>
+            <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
+              <div style={{ cursor: 'pointer' }}>
+                <DragIcon type="primary" />
+              </div>
+              <div className='ml-2'>
+                <ConstructorElement
+                  text="Краторная булка N-200i (центр)"
+                  price={50}
+                  thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
+                /></div>
+            </div>
+            <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
+              <div style={{ cursor: 'pointer' }}>
+                <DragIcon type="primary" />
+              </div>
+              <div className='ml-2'>
+                <ConstructorElement
+                  text="Краторная булка N-200i (центр)"
+                  price={50}
+                  thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
+                /></div>
+            </div>
+            <div style={{ height: '80px', display: 'flex', alignItems: 'center' }} className='mt-4'>
+              <div style={{ cursor: 'pointer' }}>
+                <DragIcon type="primary" />
+              </div>
+              <div className='ml-2'>
+                <ConstructorElement
+                  text="Краторная булка N-200i (центр)"
+                  price={50}
+                  thumbnail={'https://code.s3.yandex.net/react/code/bun-02.png'}
+                /></div>
+            </div></div>
           <div style={{ height: '80px' }} className='ml-8 mt-4'>
             <ConstructorElement
               type="bottom"
@@ -141,14 +146,23 @@ function App() {
                 Оформить заказ
               </Button>
             </div>
-            <Modal active={modalActive} setActive={setModalActive}> </Modal>
+            <Modal active={modalActive} setActive={setModalActive}>
+              <div className="close-button" onClick={() => setModalActive(false)}>
+                <CloseIcon type="primary" />
+              </div>
+              <h2 className='text text_type_digits-large mt-30'>034546</h2>
+              <p className='text text_type_main-medium mt-8 mb-15'>идентификатор заказа</p>
+              <img src={done}></img>
+              <p className='text text_type_main-medium mt-15 mb-2'>Ваш заказ начали готовить</p>
+              <p className='text text_type_main-medium text_color_inactive mb-30'>Дождитесь готовности на орбитальной станции</p>
+            </Modal>
 
           </div>
         </div>
 
       </div>
 
-    </div>
+    </div >
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Ingredients from './components/newApi';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
